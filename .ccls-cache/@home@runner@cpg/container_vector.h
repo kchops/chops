63aef8bc28cdb13e300e9313bc4a2344083b86f2
bkc <60018713+thumpro@users.noreply.github.com>
2022-11-17 21:35:09 +0000

    project structure init

--- .ccls-cache/@home@runner@cpg/container_vector.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void vector_init(void** buffer, uint64_t element_size);
-void vector_push_back(void ** buffer, const void * const elem);
-void vector_pop_back(void ** buffer);
-uint64_t vector_length(const void * const buffer);
-void * const vector_at_checked(void * const buffer, uint64_t idx);
-void * const vector_at(void * const buffer, uint64_t idx);
--- .ccls-cache/@home@runner@cpg/string.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdint.h>
-
-void string_init(char** buffer, const char* str, uint64_t size);
-uint64_t string_length(const char * const buffer);
-char string_char_at(char * const buffer, uint64_t idx);
