b332b347cb823f13e2de992c3417dad6eee7e032
thumpro <60018713+thumpro@users.noreply.github.com>
2022-11-18 16:08:02 +0100

    Update run.sh to be path agnostic

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -eux
+PWD=$(pwd)
 
 git submodule init
 git submodule update --recursive --init
@@ -10,20 +11,20 @@ cd build
 mkdir -p install
 cmake .. 
 cmake --build .
-cmake --install . --config Debug --prefix "/home/runner/chops/build/install"
+cmake --install . --config Debug --prefix "${PWD}/install"
 
 echo "---------Building the ctest library (unit test dependency)!---------"
 cd install
 cmake -S../../tests/deps/ctest -B./ctest -Duse_coloring:bool=ON
 cmake --build ./ctest
-cmake --install ./ctest --config Debug --prefix "/home/runner/chops/build/install/ctest"
-cp /home/runner/chops/build/install/ctest/libctest.a /home/runner/chops/build/install/lib
-cp /home/runner/chops/build/install/ctest/deps/c-logging/libc_logging.a /home/runner/chops/build/install/lib
+cmake --install ./ctest --config Debug --prefix "${PWD}/ctest"
+cp ${PWD}/ctest/libctest.a ${PWD}/lib
+cp ${PWD}/ctest/deps/c-logging/libc_logging.a ${PWD}/lib
 
 #build the tests
 echo "---------------------Building the chops tests!---------------------"
-export LD_LIBRARY_PATH=/home/runner/chops/install/lib:$LD_LIBRARY_PATH
-cmake ../../tests -Dchops_DIR="/home/runner/chops/build/install/lib/cmake/chops"
+export LD_LIBRARY_PATH=${PWD}/lib:$LD_LIBRARY_PATH
+cmake ../../tests -Dchops_DIR="${PWD}/lib/cmake/chops"
 cmake --build .
 
 cd ../..
