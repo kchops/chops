7ec80d570c4bbe18f49afa493879cdcdb921afb4
thumpro <60018713+thumpro@users.noreply.github.com>
2022-11-19 00:37:46 +0100

    use visual studio 2022 for compiling on ci

--- ci/ci-x64-win.ps1
+++ ci/ci-x64-win.ps1
@@ -3,5 +3,5 @@
 git submodule init
 git submodule update --recursive --init
 
-cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -S"$pwd" -B"$pwd/build" -G "Visual Studio 16 2019" -T host=x64 -A x64
+cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -S"$pwd" -B"$pwd/build" -G "Visual Studio 17 2022" -T host=x64 -A x64
 cmake --build "$pwd/build" --config Debug --target ALL_BUILD -j 14 --
