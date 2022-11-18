cd3da349ae4ee5105a9d5fccfcd2e0daeb55f1f9
bkc <60018713+thumpro@users.noreply.github.com>
2022-11-18 19:21:10 +0000

    valgrind is coredumping when run inside qemu, on arm runner, disable for now

---
+++ ci/ci-arm-linux.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -eux
+PWD=$(pwd)
+
+git submodule init
+git submodule update --recursive --init
+
+echo "---------------------Building the chops library!--------------------"
+mkdir -p build
+cd build
+mkdir -p install
+cmake .. 
+cmake --build .
+cmake --install . --config Debug --prefix "${PWD}/install"
+
+echo "---------Building the ctest library (unit test dependency)!---------"
+cd install
+cmake -S../../tests/deps/ctest -B./ctest -Duse_coloring:bool=ON
+cmake --build ./ctest
+cmake --install ./ctest --config Debug --prefix "${PWD}/ctest"
+cp ${PWD}/ctest/libctest.a ${PWD}/lib
+cp ${PWD}/ctest/deps/c-logging/libc_logging.a ${PWD}/lib
+
+#build the tests
+echo "---------------------Building the chops tests!---------------------"
+cmake ../../tests -Dchops_DIR="${PWD}/lib/cmake/chops"
+cmake --build .
+
+cd ../..
+# echo "--------------Running valgrind to check memory errors!-------------"
+# valgrind --leak-check=full --track-origins=yes build/install/chops-tests
+echo "------------------------Running unittests!-------------------------"
+./build/install/chops-tests
